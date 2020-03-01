c3946fa0d81d784c0bf924356f457b3ac83456b2
Naoki Iwasawa <niwasawa@maigo.info>
2020-03-01 17:42:50 +0900

    Support rake spec

--- spec/ydf_spec.rb
+++ spec/ydf_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Ydf do
+describe YDF do
   it 'has a version number' do
-    expect(Ydf::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(YDF::VERSION).not_to be nil
   end
 end
